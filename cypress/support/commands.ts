4ce1bda7ae38a822c955d7529f8877cec48c2afa
RAHERIMIHAJA <33063880+Nirilanto@users.noreply.github.com>
2024-04-25 14:22:51 +0200

    Initialize admin-panel-tests

---
+++ cypress/support/commands.ts
@@ -0,0 +1,78 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+Cypress.Commands.add(
+  "createNewUser",
+  ({ firstName, lastName, email, password }) => {
+    cy.request("POST", `${Cypress.env("apiUrl")}/v1/auth/email/register`, {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  }
+);
+
+Cypress.Commands.add("getBySel", (selector) => {
+  return cy.get(`[data-testid=${selector}]`);
+});
+
+Cypress.Commands.add("login", ({ email, password }) => {
+  const signinPath = "/sign-in";
+
+  cy.intercept("POST", "/api/v1/auth/email/login").as("login");
+
+  cy.location("pathname").then((currentPath) => {
+    if (currentPath !== signinPath) {
+      cy.visit(signinPath);
+    }
+  });
+
+  cy.getBySel("email").type(email);
+  cy.getBySel("password").type(password);
+  cy.getBySel("sign-in-submit").click();
+  cy.wait("@login");
+  cy.location("pathname").should("not.include", "/sign-in");
+  cy.getCookie("auth-token-data").should("exist");
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.intercept("POST", "/api/v1/auth/logout").as("logout");
+  cy.getBySel("profile-menu-item").click();
+  cy.getBySel("logout-menu-item").click();
+  cy.wait("@logout").then((request) => {
+    expect(request.response?.statusCode).to.equal(204);
+  });
+  cy.getBySel("home-page").should("be.visible");
+  cy.getBySel("sign-in").should("be.visible");
+  cy.getBySel("sign-up").should("be.visible");
+  cy.getCookie("auth-token-data").should("not.exist");
+});
+
+export {};
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      createNewUser(params: {
+        email: string;
+        password: string;
+        firstName: string;
+        lastName: string;
+      }): Chainable<void>;
+      getBySel(selector: string): Chainable<JQuery<HTMLElement>>;
+      login(params: { email: string; password: string }): Chainable<void>;
+      logout(): Chainable<void>;
+    }
+  }
+}
